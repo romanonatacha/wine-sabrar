c76d01f9d5904b45e64162f913701919cd4422e2
Natacha Romano <natacharomanonr@gmail.com>
2023-04-05 19:07:37 -0300

    fixing sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,24 +13,26 @@ const Navbar = () => {
             <div className="c-navbar__logo">
                 <img src="/images/logo-no-background.svg" alt="wine sabrar-logo" />
             </div>
-            <ul className="c-navbar__menu">
-                <li className="c-navbar__menu__item">
-                    <a href="#sabrage">sabrage</a>
-                </li>
-                <li className="c-navbar__menu__item">
-                    <a href="#social">{t.navSocial}</a>
-                </li>
-            </ul>
-            <div className="c-navbar__lang">
-                {locales.map(l => (
-                    <img
-                        key={l}
-                        onClick={() => router.push('/', '/', { locale: l })}
-                        src={l === 'pt' ? '/images/brazil.png' : '/images/usa.png'}
-                        alt={l}
-                        className={locale === l ? 'active' : ''}
-                    />
-                ))}
+            <div className="c-navbar__wrapper">
+                <ul className="c-navbar__menu">
+                    <li className="c-navbar__menu__item">
+                        <a href="#sabrage">sabrage</a>
+                    </li>
+                    <li className="c-navbar__menu__item">
+                        <a href="#social">{t.navSocial}</a>
+                    </li>
+                </ul>
+                <div className="c-navbar__lang">
+                    {locales.map(l => (
+                        <img
+                            key={l}
+                            onClick={() => router.push('/', '/', { locale: l })}
+                            src={l === 'pt' ? '/images/brazil.png' : '/images/usa.png'}
+                            alt={l}
+                            className={locale === l ? 'active' : ''}
+                        />
+                    ))}
+                </div>
             </div>
         </nav>
     )
